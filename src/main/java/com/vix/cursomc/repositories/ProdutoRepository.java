1a726b47655e13afe0c37692ab243b71b646fc29
Marcio Herhos <herhos@outlook.com>
2022-02-08 15:12:17 -0300

    Produto e associação muitos para muitos

--- src/main/java/com/vix/cursomc/repositories/ProdutoRepository.java
+++ src/main/java/com/vix/cursomc/repositories/ProdutoRepository.java
@@ -3,7 +3,7 @@ package com.vix.cursomc.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.vix.cursomc.domain.Categoria;
+import com.vix.cursomc.domain.Produto;
 
 @Repository
-public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {}
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {}

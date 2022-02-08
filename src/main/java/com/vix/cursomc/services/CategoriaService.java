dcae82b72d6f289b3e07bcda061a7f7a663b9cb1
Marcio Herhos <herhos@outlook.com>
2022-02-08 16:38:08 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/vix/cursomc/services/CategoriaService.java
+++ src/main/java/com/vix/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.vix.cursomc.domain.Categoria;
 import com.vix.cursomc.repositories.CategoriaRepository;
+import com.vix.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService
@@ -17,6 +18,8 @@ public class CategoriaService
 	public Categoria find(Integer id)
 	{
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException
+			("Objeto não encontrado! Id: " + id + ", Tipo: " + 
+			Categoria.class.getName()));
 	}
 }

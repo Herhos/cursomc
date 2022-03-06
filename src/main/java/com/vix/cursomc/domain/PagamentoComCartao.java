aae5bc9fe4ba4b671c14f57c45d6c6e3a42ff65f
Marcio Herhos <herhos@outlook.com>
2022-03-05 22:03:13 -0300

    Inserindo Pedido

--- src/main/java/com/vix/cursomc/domain/PagamentoComCartao.java
+++ src/main/java/com/vix/cursomc/domain/PagamentoComCartao.java
@@ -2,9 +2,11 @@ package com.vix.cursomc.domain;
 
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.vix.cursomc.domain.enums.EstadoPagamento;
 
 @Entity
+@JsonTypeName("pagamentoComCartao")
 public class PagamentoComCartao extends Pagamento
 {
 	private static final long serialVersionUID = 1L;
--- src/main/java/com/vix/cursomc/resources/PedidoResource.java
+++ src/main/java/com/vix/cursomc/resources/PedidoResource.java
@@ -1,11 +1,17 @@
 package com.vix.cursomc.resources;
 
+import java.net.URI;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.vix.cursomc.domain.Pedido;
 import com.vix.cursomc.services.PedidoService;
@@ -17,10 +23,23 @@ public class PedidoResource
 	@Autowired
 	private PedidoService service;
 	
+	// MÉTODO BUSCAR (GET)
+	
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
 	public ResponseEntity<Pedido> find(@PathVariable Integer id)
 	{
 		Pedido obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
 	}
+	
+	// MÉTODO INSERIR (POST)
+	
+		@RequestMapping(method = RequestMethod.POST)
+		public ResponseEntity<Void> insert(@Valid @RequestBody Pedido obj)
+		{
+			obj = service.insert(obj);
+			URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+			return ResponseEntity.created(uri).build();
+		}
 }
